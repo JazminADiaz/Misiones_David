75dd092d4a471c682895dcbcf727349a0c1222f9
Jazmin Ubuntu <jazminarianadiaz@gmail.com>
2023-07-20 00:31:08 -0500

    no funciona pero esa es la idea

--- experiments-loop-functions/loop-functions/moca/TuttiTmLoopFunc.cpp
+++ experiments-loop-functions/loop-functions/moca/TuttiTmLoopFunc.cpp
@@ -222,113 +222,241 @@ Real TuttiTmLoopFunction::GetMoveScore() {
 }
 
 Real TuttiTmLoopFunction::TamControl() {
-    UInt32 tam1=UpdateRobotPositions();
-    
+    CSpace::TMapPerType& tBlocksMap = GetSpace().GetEntitiesByType("box");
+    CVector2 cBoxPosition(0,0), Tam1(0,0), Tam2(0,0), Tam3(0,0), Tam4(0,0), Tam5(0,0), Tam6(0,0);
+    Real y_l=0.11,x_l=0.08, rob=0;
+    UInt32 unBlocksID = 0;
+    std::string action, robot;
 
-    
-    InitRobotStates();
-    //m_pcArena->SetArenaColor(CColor::BLACK);
+    for (CSpace::TMapPerType::iterator it = tBlocksMap.begin(); it != tBlocksMap.end(); ++it) {
+        CBoxEntity* pcBlock = any_cast<CBoxEntity*>(it->second);
+        cBoxPosition.Set(pcBlock->GetEmbodiedEntity().GetOriginAnchor().Position.GetX(),
+                        pcBlock->GetEmbodiedEntity().GetOriginAnchor().Position.GetY());
 
-    //if (m_unClock == m_unStopTime) {
-        CSpace::TMapPerType& tBlocksMap = GetSpace().GetEntitiesByType("box");
-        UInt32 unBlocksID = 0;
+            if (unBlocksID ==0){Tam1.SetX(cBoxPosition.GetX());}
+            if (unBlocksID ==1){Tam1.SetY(cBoxPosition.GetY());}
+            if (unBlocksID ==3){Tam2.SetX(cBoxPosition.GetX());}
+            if (unBlocksID ==4){Tam2.SetY(cBoxPosition.GetY());}
+            if (unBlocksID ==6){Tam3.SetX(cBoxPosition.GetX());}
+            if (unBlocksID ==7){Tam3.SetY(cBoxPosition.GetY());}
+            if (unBlocksID ==9){Tam4.SetX(cBoxPosition.GetX());}
+            if (unBlocksID ==10){Tam4.SetY(cBoxPosition.GetY());}
+            if (unBlocksID ==12){Tam5.SetX(cBoxPosition.GetX());}
+            if (unBlocksID ==13){Tam5.SetY(cBoxPosition.GetY());}
+            if (unBlocksID ==15){Tam6.SetX(cBoxPosition.GetX());}
+            if (unBlocksID ==16){Tam6.SetY(cBoxPosition.GetY());}
+        unBlocksID += 1; 
+    }
+
+    CSpace::TMapPerType& tEpuckMap = GetSpace().GetEntitiesByType("epuck");
+    CVector2 cEpuckPosition(0,0);
+    UInt32  tam1=0, tam2=0, tam3=0, tam4=0, tam5=0, tam6=0;
+ for (CSpace::TMapPerType::iterator it = tEpuckMap.begin(); it != tEpuckMap.end(); ++it) {
+        CEPuckEntity* pcEpuck = any_cast<CEPuckEntity*>(it->second);
+        cEpuckPosition.Set(pcEpuck->GetEmbodiedEntity().GetOriginAnchor().Position.GetX(),
+                           pcEpuck->GetEmbodiedEntity().GetOriginAnchor().Position.GetY());
+
+        m_tRobotStates[pcEpuck].cLastPosition = m_tRobotStates[pcEpuck].cPosition;
+
+        m_tRobotStates[pcEpuck].cPosition = cEpuckPosition;
+
+        robot=std::to_string(rob);
+//tam1 
+    if (Tam1.GetY()-y_l< cEpuckPosition.GetY() and cEpuckPosition.GetY() <= Tam1.GetY()
+    and (Tam1.GetX())-x_l< cEpuckPosition.GetX() and cEpuckPosition.GetX() <= Tam1.GetX()
+    ){ 
+        action=";T1_enter";
+        action=robot+action;
+        if (abs((m_tRobotStates[pcEpuck].cPosition.GetX())- m_tRobotStates[pcEpuck].cLastPosition.GetX())<=0.000005 and
+         abs((m_tRobotStates[pcEpuck].cPosition.GetY())- m_tRobotStates[pcEpuck].cLastPosition.GetY())<=0.000005 and tam1==0){
+            action=";T1_stop";
+            action=robot+action;
+            tam1=1;
+        }
+        
+
+    }
+//tam2
+    if (Tam2.GetY()-y_l< cEpuckPosition.GetY() and cEpuckPosition.GetY() <= Tam2.GetY()
+    and (Tam2.GetX()-x_l)< cEpuckPosition.GetX() and cEpuckPosition.GetX() <= Tam2.GetX()
+    ){  action=";T2_enter";
+        action=robot+action;
+        if (abs((m_tRobotStates[pcEpuck].cPosition.GetX())- m_tRobotStates[pcEpuck].cLastPosition.GetX())<=0.000005 and
+         abs((m_tRobotStates[pcEpuck].cPosition.GetY())- m_tRobotStates[pcEpuck].cLastPosition.GetY())<=0.000005 and tam2==0){
+            action=";T2_stop";
+            action=robot+action;
+            tam2=1;
+        }
+             }
+
+//tam3
+    if (Tam3.GetY()-y_l< cEpuckPosition.GetY() and cEpuckPosition.GetY() <= Tam3.GetY()
+    and (Tam3.GetX()-x_l)< cEpuckPosition.GetX() and cEpuckPosition.GetX() <= Tam3.GetX()
+    ){  action=";T3_enter";
+        action=robot+action;
+        if (abs((m_tRobotStates[pcEpuck].cPosition.GetX())- m_tRobotStates[pcEpuck].cLastPosition.GetX())<=0.000005 and
+         abs((m_tRobotStates[pcEpuck].cPosition.GetY())- m_tRobotStates[pcEpuck].cLastPosition.GetY())<=0.000005 and tam3==0){
+            action=";T3_stop";
+            action=robot+action;
+            tam3=1;
+        }   
+             }
+//tam4
+    if (Tam4.GetY()< cEpuckPosition.GetY() and cEpuckPosition.GetY() <= Tam4.GetY()+y_l
+    and (Tam4.GetX())-x_l< cEpuckPosition.GetX() and cEpuckPosition.GetX() <= Tam4.GetX()
+    ){  action=";T4_enter";
+        action=robot+action;
+        if (abs((m_tRobotStates[pcEpuck].cPosition.GetX())- m_tRobotStates[pcEpuck].cLastPosition.GetX())<=0.000005 and
+         abs((m_tRobotStates[pcEpuck].cPosition.GetY())- m_tRobotStates[pcEpuck].cLastPosition.GetY())<=0.000005 and tam4==0){
+            action=";T4_stop";
+            action=robot+action;
+            tam4=1; 
+        }
+            }
+//tam5
+    if (Tam5.GetY()< cEpuckPosition.GetY() and cEpuckPosition.GetY() <= Tam5.GetY()+y_l
+    and (Tam5.GetX()-x_l)< cEpuckPosition.GetX() and cEpuckPosition.GetX() <= Tam5.GetX()
+    ){  
+        action=";T5_enter";
+        action=robot+action;
+        if (abs((m_tRobotStates[pcEpuck].cPosition.GetX())- m_tRobotStates[pcEpuck].cLastPosition.GetX())<=0.000005 and
+         abs((m_tRobotStates[pcEpuck].cPosition.GetY())- m_tRobotStates[pcEpuck].cLastPosition.GetY())<=0.000005 and tam5==0){
+            action=";T5_stop";
+            action=robot+action;
+            tam5=1;
+        }
+           }
+//tam6
+    if (Tam6.GetY()< cEpuckPosition.GetY() and cEpuckPosition.GetY() <= Tam6.GetY()+y_l
+    and (Tam6.GetX()-x_l)< cEpuckPosition.GetX() and cEpuckPosition.GetX() <= Tam6.GetX()
+    ){  action=";T5_enter";
+        action=robot+action;
+        if (abs((m_tRobotStates[pcEpuck].cPosition.GetX())- m_tRobotStates[pcEpuck].cLastPosition.GetX())<=0.000005 and
+         abs((m_tRobotStates[pcEpuck].cPosition.GetY())- m_tRobotStates[pcEpuck].cLastPosition.GetY())<=0.000005 and tam6==0){
+            action=";T6_stop";
+            action=robot+action;
+            tam6=1;
+        }
+          }
+
+    rob +=1;
+
+    }
+//        CSpace::TMapPerType& tBlocksMap = GetSpace().GetEntitiesByType("box");
+        UInt32 unBlocksID1 = 0;
         for (CSpace::TMapPerType::iterator it = tBlocksMap.begin(); it != tBlocksMap.end(); ++it) {
             CBoxEntity* pcBlock = any_cast<CBoxEntity*>(it->second);
-            //std::cout << unBlocksID << std::endl;
-            //pcBlock->GetLEDEquippedEntity().SetAllLEDsColors(CColor::MAGENTA);
-            std::cout<<"tam:"<<tam1<<"bloque"<<unBlocksID<<std::endl;
+
+//            std::cout<<"tam:"<<tam1<<"bloque"<<unBlocksID<<std::endl;
             
-            if ((unBlocksID == 6 or unBlocksID == 9) and tam1==2) {
-                pcBlock->GetLEDEquippedEntity().SetAllLEDsColors(CColor::BLUE);
+            if (unBlocksID1 ==0 and tam1==1) {
+                pcBlock->GetLEDEquippedEntity().SetAllLEDsColors(CColor::MAGENTA);
             }
-            if ((unBlocksID == 12 or unBlocksID == 15) and tam1==4) {
+
+            if (unBlocksID1 ==3 and tam2==1) {
+                pcBlock->GetLEDEquippedEntity().SetAllLEDsColors(CColor::MAGENTA);
+            }
+
+            if ((unBlocksID1 == 6 or unBlocksID1 == 9) and tam1==1 and tam2==1) {
                 pcBlock->GetLEDEquippedEntity().SetAllLEDsColors(CColor::BLUE);
             }
 
-            unBlocksID += 1;    
+
+            if (unBlocksID1 == 6 and tam3==1) {
+                pcBlock->GetLEDEquippedEntity().SetAllLEDsColors(CColor::MAGENTA);
+                
+            }
+            if (unBlocksID1 == 9 and tam4==1) {
+                pcBlock->GetLEDEquippedEntity().SetAllLEDsColors(CColor::MAGENTA);   
+            }  
+            if ((unBlocksID1 == 12 or unBlocksID1 == 15) and tam3==1 and tam4==1) {
+                pcBlock->GetLEDEquippedEntity().SetAllLEDsColors(CColor::BLUE);
+                
+            }
+            if ((unBlocksID1 == 12 or unBlocksID1 == 15) and tam5==1 and tam6==1) {
+                pcBlock->GetLEDEquippedEntity().SetAllLEDsColors(CColor::MAGENTA);
+            }
+
+            unBlocksID1 += 1;    
         }
-    
-    return tam;
-
-
 }
 
 
 /****************************************/
 /****************************************/
+
+///CONFIGURE FLOOR COLOR ACORDING TO TAM LOCATIONS 
 
 argos::CColor TuttiTmLoopFunction::GetFloorColor(const argos::CVector2& c_position_on_plane) {
     
-    //tam1
-    if (-0.05< c_position_on_plane.GetY() and c_position_on_plane.GetY() <= 0.05 
-    and 0.68< c_position_on_plane.GetX() and c_position_on_plane.GetX() <= 0.75
-    ){
-        
-        return CColor::WHITE;
-    }
-//tam2
-    if (-0.25< c_position_on_plane.GetY() and c_position_on_plane.GetY() <= -0.15 
 
-    and 0.68< c_position_on_plane.GetX() and c_position_on_plane.GetX() <= 0.75
-    ){
-        return CColor::WHITE;
-    }
-    
-    //else if (c_position_on_plane.GetX() >= 0.60){
-    //    return CColor::BLACK;
-    //}
-//tam2
-    if (-0.45< c_position_on_plane.GetY() and c_position_on_plane.GetY() <= -0.35 
+        CSpace::TMapPerType& tBlocksMap = GetSpace().GetEntitiesByType("box");
+        CVector2 cBoxPosition(0,0), Tam1(0,0), Tam2(0,0), Tam3(0,0), Tam4(0,0), Tam5(0,0), Tam6(0,0);
+        Real y_l=0.1, x_l=0.07;
 
-    and 0.68< c_position_on_plane.GetX() and c_position_on_plane.GetX() <= 0.75
-    ){
-        return CColor::WHITE;
-    }
+        UInt32 unBlocksID = 0;
+        for (CSpace::TMapPerType::iterator it = tBlocksMap.begin(); it != tBlocksMap.end(); ++it) {
+            CBoxEntity* pcBlock = any_cast<CBoxEntity*>(it->second);
+            cBoxPosition.Set(pcBlock->GetEmbodiedEntity().GetOriginAnchor().Position.GetX(),
+                           pcBlock->GetEmbodiedEntity().GetOriginAnchor().Position.GetY());
+
+                if (unBlocksID ==0){Tam1.SetX(cBoxPosition.GetX());}
+                if (unBlocksID ==1){Tam1.SetY(cBoxPosition.GetY());}
+                if (unBlocksID ==3){Tam2.SetX(cBoxPosition.GetX());}
+                if (unBlocksID ==4){Tam2.SetY(cBoxPosition.GetY());}
+                if (unBlocksID ==6){Tam3.SetX(cBoxPosition.GetX());}
+                if (unBlocksID ==7){Tam3.SetY(cBoxPosition.GetY());}
+                if (unBlocksID ==9){Tam4.SetX(cBoxPosition.GetX());}
+                if (unBlocksID ==10){Tam4.SetY(cBoxPosition.GetY());}
+                if (unBlocksID ==12){Tam5.SetX(cBoxPosition.GetX());}
+                if (unBlocksID ==13){Tam5.SetY(cBoxPosition.GetY());}
+                if (unBlocksID ==15){Tam6.SetX(cBoxPosition.GetX());}
+                if (unBlocksID ==16){Tam6.SetY(cBoxPosition.GetY());}
+
+            unBlocksID += 1; 
+        }
+//tam1 
+    if (Tam1.GetY()-y_l< c_position_on_plane.GetY() and c_position_on_plane.GetY() <= Tam1.GetY()
+    and (Tam1.GetX())-x_l< c_position_on_plane.GetX() and c_position_on_plane.GetX() <= Tam1.GetX()
+    ){          return CColor::WHITE;    }
+//tam2
+    if (Tam2.GetY()-y_l< c_position_on_plane.GetY() and c_position_on_plane.GetY() <= Tam2.GetY()
+    and (Tam2.GetX()-x_l)< c_position_on_plane.GetX() and c_position_on_plane.GetX() <= Tam2.GetX()
+    ){  return CColor::WHITE;    }
+
+//tam3
+    if (Tam3.GetY()-y_l< c_position_on_plane.GetY() and c_position_on_plane.GetY() <= Tam3.GetY()
+    and (Tam3.GetX()-x_l)< c_position_on_plane.GetX() and c_position_on_plane.GetX() <= Tam3.GetX()
+    ){   return CColor::WHITE;    }
 //tam4
-    if (-0.65< c_position_on_plane.GetY() and c_position_on_plane.GetY() <= -0.55 
-
-    and 0.68< c_position_on_plane.GetX() and c_position_on_plane.GetX() <= 0.75
-    ){
-        return CColor::WHITE;
-    }
+    if (Tam4.GetY()< c_position_on_plane.GetY() and c_position_on_plane.GetY() <= Tam4.GetY()+y_l
+    and (Tam4.GetX())-x_l< c_position_on_plane.GetX() and c_position_on_plane.GetX() <= Tam4.GetX()
+    ){ return CColor::WHITE;    }
 //tam5
-    if (0.15< c_position_on_plane.GetY() and c_position_on_plane.GetY() <= 0.25 
-
-    and 0.68< c_position_on_plane.GetX() and c_position_on_plane.GetX() <= 0.75
-    ){
-        return CColor::WHITE;
-    }
-
+    if (Tam5.GetY()< c_position_on_plane.GetY() and c_position_on_plane.GetY() <= Tam5.GetY()+y_l
+    and (Tam5.GetX()-x_l)< c_position_on_plane.GetX() and c_position_on_plane.GetX() <= Tam5.GetX()
+    ){  return CColor::WHITE;}
 //tam6
-    if (0.35< c_position_on_plane.GetY() and c_position_on_plane.GetY() <= 0.45 
-
-    and 0.68< c_position_on_plane.GetX() and c_position_on_plane.GetX() <= 0.75
-    ){
-        return CColor::WHITE;
-    }
-
-    
-
-//tam7
-    //if (0.55< c_position_on_plane.GetY() and c_position_on_plane.GetY() <= 0.65 
-
-    //and 0.68< c_position_on_plane.GetX() and c_position_on_plane.GetX() <= 0.75)
-    //{        return CColor::WHITE;    }
-
-if (0.45< c_position_on_plane.GetX()){
-
-            return CColor::BLACK;}
+    if (Tam6.GetY()< c_position_on_plane.GetY() and c_position_on_plane.GetY() <= Tam6.GetY()+y_l
+    and (Tam6.GetX()-x_l)< c_position_on_plane.GetX() and c_position_on_plane.GetX() <= Tam6.GetX()
+    ){  return CColor::WHITE;}
+//else
+    if (0.45< c_position_on_plane.GetX()){return CColor::BLACK;}
 
     return CColor::GRAY50;
 }
+//END FLOOR
 
 /****************************************/
 /****************************************/
 
+//
 Real TuttiTmLoopFunction::UpdateRobotPositions() {
     CSpace::TMapPerType& tEpuckMap = GetSpace().GetEntitiesByType("epuck");
-    int tam=0;
     CVector2 cEpuckPosition(0,0);
+    
+
+
     for (CSpace::TMapPerType::iterator it = tEpuckMap.begin(); it != tEpuckMap.end(); ++it) {
         CEPuckEntity* pcEpuck = any_cast<CEPuckEntity*>(it->second);
         cEpuckPosition.Set(pcEpuck->GetEmbodiedEntity().GetOriginAnchor().Position.GetX(),
@@ -340,16 +468,10 @@ Real TuttiTmLoopFunction::UpdateRobotPositions() {
         m_tRobotStates[pcEpuck].cLastPosition = m_tRobotStates[pcEpuck].cPosition;
 
         m_tRobotStates[pcEpuck].cPosition = cEpuckPosition;
-        
-
-        if (abs((m_tRobotStates[pcEpuck].cPosition.GetX())- m_tRobotStates[pcEpuck].cLastPosition.GetX())<=0.000005 and
-        abs((m_tRobotStates[pcEpuck].cPosition.GetY())- m_tRobotStates[pcEpuck].cLastPosition.GetY())<=0.000005){
-            tam+=1;
-        }
-
 
     }
-    return tam;
+    
+;
 }
 
 
