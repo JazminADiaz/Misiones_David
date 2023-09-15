bfa04b0d39e5966101c98053f191a1348aa7f703
Jazmin Ubuntu <jazminarianadiaz@gmail.com>
2023-09-14 19:14:27 -0500

    escenario con tams a los lados son islas aún

--- log/Tesis/Codigos/argos.py
+++ log/Tesis/Codigos/argos.py
@@ -22,7 +22,8 @@ def generate_argos_wall(length):
                     t_move1=half_length-size_division-tam_size/2.0
                     t_move2=half_length-size_division
                     t_move3=half_length-size_division-tam_size
-
+                    estatic1=half_length+tam_size
+                    estatic2=half_length+tam_size/2.0
 
                 xml_code += f"""<!-- * Wall{wall_id}_{wall_division} -->
                 <box id="wall_i{wall_id}_{wall_division}" movable="false" size="0.01,{size_division},0.09">
@@ -48,9 +49,9 @@ def generate_argos_wall(length):
                     """
                 guarda_lat=move-size_division/2.0
                 move+=adding
-                t_move1+=+adding
-                t_move2+=+adding
-                t_move3+=+adding
+                t_move1+=adding
+                t_move2+=adding
+                t_move3+=adding
 
 
             if (wall_id==2):
@@ -59,6 +60,9 @@ def generate_argos_wall(length):
                     t_move1=half_length-size_division-tam_size/2.0
                     t_move2=half_length-size_division
                     t_move3=half_length-size_division-tam_size
+                    estatic1=half_length+tam_size
+                    estatic2=half_length+tam_size/2.0
+
                 xml_code += f"""<!-- * Wall_{wall_id}_{wall_division}-->
                 <box id="wall_i{wall_id}_{wall_division}" movable="false" size="0.01,{size_division},0.09">
                 <body orientation="90,0,0" position="{move},{half_length},0"></body>
@@ -81,32 +85,82 @@ def generate_argos_wall(length):
                         </box>
                     """
                 guarda_up=move-size_division/2.0
-                move=move+adding
-                t_move1+=+adding
-                t_move2+=+adding
-                t_move3+=+adding
+                move+=adding
+                t_move1+=adding
+                t_move2+=adding
+                t_move3+=adding
 
 
             if (wall_id==3):
                 if (wall_division==1):
                     move=half_length-size_division/2.0
+                    t_move1=half_length-size_division-tam_size/2.0
+                    t_move2=half_length-size_division
+                    t_move3=half_length-size_division-tam_size
+                    estatic1=guarda_up-tam_size
+                    estatic2=guarda_up-tam_size/2.0
                 xml_code += f"""<!-- * Wall{wall_id}_{wall_division} -->
                 <box id="wall_i{wall_id}_{wall_division}" movable="false" size="0.01,{size_division},0.09">
                 <body orientation="0,0,0" position="{guarda_lat},{move},0"></body>
                 </box>"""
 
-                move=move+adding
+                if (wall_division<division-1):
+                    xml_code += f"""<!-- * TAMS_{wall_id}_{wall_division}-->    
+                        <box id="tam_{wall_id}_{wall_division}_1" movable="false" size="0.01,0.1,0.12">
+                        <body orientation="0,0,0" position="{estatic1},{t_move1},0"></body>
+                        <leds medium="leds">
+                        <led anchor="origin" color="green" id="led_{wall_id}_{wall_division}" intensity="100.0" offset="0.01,0,0.08"></led>
+                        <led anchor="origin" color="green" id="led_{wall_id}_{wall_division}_2" intensity="100.0" offset="0.01,0.04,0.08"></led>
+                        <led anchor="origin" color="green" id="led_{wall_id}_{wall_division}_3" intensity="100.0" offset="0.01,-0.04,0.08"></led>
+                        </leds>
+                        </box>
+                        <box id="tam_{wall_id}_{wall_division}_2" movable="false" size="0.01,0.11,0.12">
+                        <body orientation="90,0,0" position="{estatic2},{t_move2},0"></body>
+                        </box>
+                        <box id="tam_{wall_id}_{wall_division}_3" movable="false" size="0.01,0.11,0.12">
+                        <body orientation="90,0,0" position="{estatic2},{t_move3},0"></body>
+                        </box>
+                    """
+                move+=adding
+                t_move1+=adding
+                t_move2+=adding
+                t_move3+=adding
+
             if (wall_id==4):
                 if (wall_division==1):
                     move=half_length-size_division/2.0
+                    t_move1=half_length-size_division-tam_size/2.0
+                    t_move2=half_length-size_division
+                    t_move3=half_length-size_division-tam_size
+                    estatic1=guarda_lat-tam_size
+                    estatic2=guarda_lat-tam_size/2.0
+
                 xml_code += f"""<!-- * Wall_{wall_id}_{wall_division}-->
                 <box id="wall_i{wall_id}_{wall_division}" movable="false" size="0.01,{size_division},0.09">
                 <body orientation="90,0,0" position="{move},{guarda_up},0"></body>
                 </box>"""
             
-                move=move+adding
-
-
+                if (wall_division<division-1):
+                    xml_code += f"""<!-- * TAMS_{wall_id}_{wall_division}-->    
+                        <box id="tam_{wall_id}_{wall_division}_1" movable="false" size="0.01,0.1,0.12">
+                        <body orientation="90,0,0" position="{t_move1},{estatic1},0"></body>
+                        <leds medium="leds">
+                        <led anchor="origin" color="green" id="led_{wall_id}_{wall_division}" intensity="100.0" offset="0.01,0,0.08"></led>
+                        <led anchor="origin" color="green" id="led_{wall_id}_{wall_division}_2" intensity="100.0" offset="0.01,0.04,0.08"></led>
+                        <led anchor="origin" color="green" id="led_{wall_id}_{wall_division}_3" intensity="100.0" offset="0.01,-0.04,0.08"></led>
+                        </leds>
+                        </box>
+                        <box id="tam_{wall_id}_{wall_division}_2" movable="false" size="0.01,0.11,0.12">
+                        <body orientation="0,0,0" position="{t_move2},{estatic2},0"></body>
+                        </box>
+                        <box id="tam_{wall_id}_{wall_division}_3" movable="false" size="0.01,0.11,0.12">
+                        <body orientation="0,0,0" position="{t_move3},{estatic2},0"></body>
+                        </box>
+                    """
+                move+=adding
+                t_move1+=adding
+                t_move2+=adding
+                t_move3+=adding
 
     return xml_code
 '''
