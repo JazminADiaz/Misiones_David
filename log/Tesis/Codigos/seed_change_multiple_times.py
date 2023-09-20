e7177f5c0039d2bbb18ac4a092c38d09429bfcb5
Jazmin Ubuntu <jazminarianadiaz@gmail.com>
2023-09-20 12:33:40 -0500

    no funciona seed change

--- log/Tesis/Codigos/seed_change_multiple_times.py
+++ log/Tesis/Codigos/seed_change_multiple_times.py
@@ -12,19 +12,19 @@ def simulacion(directorio, repetition, time):
     for f in filelist:
         os.remove(f)
 
-    for b in range(1, repetition + 1):
+    for be in range(1, repetition + 1):
         with open(f"/home/jazmin/tuttifrutti/experiments-loop-functions/scenarios/tuttifrutti/{mision}.argos") as fp:
             soup = bs(fp, 'html.parser')
         
         tag = soup.experiment
-        tag['random_seed'] = b
+        tag['random_seed'] = be
         
         with open("/home/jazmin/tuttifrutti/log/Tesis/mision_variables/seed.txt", "w"):
             pass
         
         with open("/home/jazmin/tuttifrutti/log/Tesis/mision_variables/seed.txt", "a") as f:
             f.write("\n")
-            f.write(f"s= \"{b}\";\n")
+            f.write(f"s= \"{be}\";\n")
             f.write(f"events_folder= \"{directorio}\";\n")
             f.write(f"timer_simulation= {time};\n")
         
@@ -44,8 +44,8 @@ def main():
     parser.add_argument('--time', type=int, default=0, help='Valor de time (por defecto: 0)')
     args = parser.parse_args()
 
-    simulacion("/home/jazmin/tuttifrutti/log/Tesis/Codigos/seed_change_multiple_times.py", 2, 2000)
-    #simulacion(args.directorio, args.repetition, args.time)
+    
+    simulacion(args.directorio, args.repetition, args.time)
     print("Simulación completada.")
 
 if __name__ == "__main__":
